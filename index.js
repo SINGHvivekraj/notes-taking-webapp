b06194fe5e37653d3cf461eebcef3d28cc3ff839
viveksingh <viveks98765@gmail.com>
2025-05-11 12:17:32 +0530

    created a notes taking web app using nodejs and express

---
+++ index.js
@@ -0,0 +1,37 @@
+const express=require('express');
+const app=express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static('public'));
+app.set('view engine','ejs');
+
+const fs=require('fs');
+
+app.get('/',(req,res)=>{
+    fs.readdir('./files',(err,files)=>{
+    res.render('index',{allfiles:files});
+})
+})
+
+app.get('/file/:filename',(req,res)=>{
+    fs.readFile(`./files/${req.params.filename}`,'utf-8',(err,filedata)=>{
+        res.render('show',{filename:req.params.filename,filedata:filedata});
+    });
+})
+
+app.post('/create',(req,res)=>{
+    const {title,details}=req.body;
+    fs.writeFile(`./files/${title.split(' ').join('')}.txt`,details,(err)=>{
+        if(err)console.error(err);
+        else console.log('task added')
+    })
+    res.redirect("/");
+
+})
+
+
+
+
+app.listen(3000);
