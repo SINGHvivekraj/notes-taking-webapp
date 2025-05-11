2cee4ee56ef325a5fe7f0d8fa1751263d359e5a1
viveksingh <viveks98765@gmail.com>
2025-05-11 15:46:04 +0530

    implemented edit filename feature using fs.rename()

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
-const express=require('express');
-const app=express();
+const express = require("express");
+const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
-app.set('view engine','ejs');
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-const fs=require('fs');
+const fs = require("fs");
 
-app.get('/',(req,res)=>{
-    fs.readdir('./files',(err,files)=>{
-    res.render('index',{allfiles:files});
-})
-})
+app.get("/", (req, res) => {
+  fs.readdir("./files", (err, files) => {
+    res.render("index", { allfiles: files });
+  });
+});
 
-app.get('/file/:filename',(req,res)=>{
-    fs.readFile(`./files/${req.params.filename}`,'utf-8',(err,filedata)=>{
-        res.render('show',{filename:req.params.filename,filedata:filedata});
-    });
-})
+app.get("/file/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    res.render("show", { filename: req.params.filename, filedata: filedata });
+  });
+});
 
-app.post('/create',(req,res)=>{
-    const {title,details}=req.body;
-    fs.writeFile(`./files/${title.split(' ').join('')}.txt`,details,(err)=>{
-        if(err)console.error(err);
-        else console.log('task added')
-    })
-    res.redirect("/");
+app.get("/edit/:filename", (req, res) => {
+  res.render("editpage", { filename: req.params.filename });
+});
 
-})
+app.post("/create", (req, res) => {
+  const { title, details } = req.body;
+  fs.writeFile(`./files/${title.split(" ").join("")}.txt`, details, (err) => {
+    if (err) console.error(err);
+    else console.log("task added");
+  });
+  res.redirect("/");
+});
 
-
-
+app.post("/edit", (req, res) => {
+  const { oldname, newname } = req.body;
+  console.log(req.body);
+  fs.rename(`./files/${oldname}`, `./files/${newname}`, (err) => {
+    if (err) console.error(err);
+    else res.redirect("/");
+  });
+});
 
 app.listen(3000);
